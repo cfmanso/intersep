a7be667e097b892d2ccb93741e2ab4996ce66bd4
Carolina Manso <carolcarolmanso@gmail.com>
2019-10-29 11:54:40 -0300

    Começando a seção conheça

--- themes/Nemo/sections/section-conheca.php
+++ themes/Nemo/sections/section-conheca.php
@@ -1,7 +1,15 @@
 <div id="conheca" class="">
 
     <div class="conheca-quadro">
-        
+        <div class="titulo-conheca"> Conheça o projeto </div>
+
+        <div class="linha">
+            <div class="celula"> 
+                <img class="img-retangular" src="<?php echo get_stylesheet_directory_uri();?>/assets/img/mini-logo.svg">
+                <div class="subtitulo-conheca"> Intersep </div>
+                <div class="text-conhece">Texto </div>
+            </div>
+        </div>
     </div>
 
 
