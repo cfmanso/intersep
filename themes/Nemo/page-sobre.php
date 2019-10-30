142001a751a89cfb44f9cfce967769169418af62
stiamat <38559112+stiamat@users.noreply.github.com>
2019-10-30 13:56:50 -0300

    Update page-sobre.php

--- themes/Nemo/page-sobre.php
+++ themes/Nemo/page-sobre.php
@@ -45,6 +45,27 @@ get_header();
                     <p>
                         O projeto INTERSEP visa atacar esta questão por meio de uma abordagem de governança de informações de Segurança Pública com base em Modelos de Informação de Referência. Tais modelos, também tecnicamente chamados de Ontologias, são gabaritos que guiam o processo de mapeamento de entidades e eventos de um silo de informação (base de dados ou sistemas de informação) em outros silos, possibilitando assim uma visão integrada da informação, que de outra forma estaria dispersa em vários silos. 
                     </p>
+
+                    <div class="row canva-default" style="margin:0px;">
+                        <!-- <div class="col-1"></div> -->
+                        <div class="col canva-inf-archive">
+                            <p class="text-principal">
+                            Resultados já alcançados
+                            </p>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <ol>
+                            <li>Análise do domínio da segurança pública, com foco no processo do crime doloso contra a vida – PCDV (incluindo as etapas de ocorrência policial, investigação, indiciamento, denúncia, julgamento, execução penal e cumprimento da pena), por meio de visitas às agências de segurança pública (SESP, SEJUS, TJ-ES e MP-ES) e análise de documentação e de sistemas de informação; </li>
+                            <li>Modelagem da arquitetura corporativa associada ao PCDV, por meio da análise da infraestrutura dos Sistemas de Informação que apoiam o processo (ex.: DEON, SIEP, INFOPEN) e os dados que fluem através dos processos (ex.: Boletim de Ocorrência, Inquérito Policial, Ficha de Execução Penal), de modo a prover um diagnóstico dos problemas de interoperabilidade (integração de dados) no PCDV; </li>
+                            <li>Modelagem de uma Rede de Modelos de Informação de Referência do PCDV, chamada PCDV-ON, contendo modelos que descrevem os conceitos relativos à ocorrência, investigação, denúncia, julgamento e cumprimento de pena, todos eles centrados em um modelo sobre descrição do crime doloso contra a vida. A partir da PCDV-ON é possível representar de forma precisa os dados relativos ao PCDV espalhados nos diversos sistemas de informação das agências de segurança pública;</li>
+                            <li>Mapeamento dos dados dos Sistemas de Informação para a PCDV-ON, associando elementos dos bancos de dados dos sistemas das agências de segurança pública (em particular, o DEON na SESP e o SIEP no TJ-ES) com os conceitos da PCDV-ON. Assim, o modelo de referência funciona como uma “interlíngua” entre os sistemas, que passam a poder interoperar de forma bem fundamentada; </li>
+                            <li>Implementação de um protótipo demonstrando uma integração e visão unificada sobre os dados distribuídos em vários silos, que permite a um gestor do Governo do ES obter respostas a questões que são possíveis apenas a partir da integração bem fundamentada dos dados dos diversos sistemas de informação do PCDV. O protótipo, disponível em http://dev.nemo.inf.ufes.br:8180/intersep/ (usuário: admin, senha: admin), utiliza dados fictícios, porém é baseado na estrutura real dos bancos de dados do DEON e do SIEP; </li>
+                            <li>Publicações científicas com os resultados do projeto. </li>
+                        </ol>
+                        <button class="btn btn-default" type="button">Click Me!</button> 
+                    </div>
+
                 </div>
 
             </div>
