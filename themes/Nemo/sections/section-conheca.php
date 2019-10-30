47524bb0d3ced0d77140a89efb5ce1e232bc4ca6
Carolina Manso <carolcarolmanso@gmail.com>
2019-10-30 11:53:39 -0300

    Página principal quase pronta

--- themes/Nemo/sections/section-conheca.php
+++ themes/Nemo/sections/section-conheca.php
@@ -1,15 +1,67 @@
 <div id="conheca" class="">
 
-    <div class="conheca-quadro">
-        <div class="titulo-conheca"> Conheça o projeto </div>
-
-        <div class="linha">
-            <div class="celula"> 
-                <img class="img-retangular" src="<?php echo get_stylesheet_directory_uri();?>/assets/img/mini-logo.svg">
-                <div class="subtitulo-conheca"> Intersep </div>
-                <div class="text-conhece">Texto </div>
+    <div class="conheca-quadro" style="    background-color: transparent;">
+        <div class="titulo-conheca" class="container"> Conheça o projeto </div>
+            <div class="card-deck">
+                <div class="card">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/mini-logo.svg" class="img-retangular">
+                    <div class="card-body">
+                    <h5 class="subtitulo-conheca">Intersep </h5>
+                    <div class="hr-default"> <hr class="hr-principal"></div>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/network" class="img-quadrada">
+                    <div class="card-body">
+                    <h5 class="subtitulo-conheca">Membros</h5>
+                    <div class="hr-default"> <hr class="hr-principal"></div>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/publications" class="img-retangular">
+                    <div class="card-body">
+                    <h5 class="subtitulo-conheca">Publicações</h5>
+                    <div class="hr-default"> <hr class="hr-principal"></div>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div class="card-deck">
+                <div class="card">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/connection.svg" class="img-quadrada">
+                    <div class="card-body">
+                    <h5 class="subtitulo-conheca">Ontologia</h5>
+                    <div class="hr-default"> <hr class="hr-principal"></div>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/presentation" class="img-quadrada">
+                    <div class="card-body">
+                    <h5 class="subtitulo-conheca">Abordagens</h5>
+                    <div class="hr-default"> <hr class="hr-principal"></div>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/worker" class="img-quadrada">
+                    <div class="card-body">
+                    <h5 class="subtitulo-conheca">Protótipo</h5>
+                    <div class="hr-default"> <hr class="hr-principal"></div>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/discussion" class="img-quadrada">
+                    <div class="card-body">
+                    <h5 class="subtitulo-conheca">Contato</h5>
+                    <div class="hr-default"> <hr class="hr-principal"></div>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+            </div>
     </div>
 
 
