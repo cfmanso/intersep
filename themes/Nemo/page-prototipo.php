5a85793c41c9f6a99b01bb9d4b59b61ef98953c5
stiamat <38559112+stiamat@users.noreply.github.com>
2019-10-30 14:28:12 -0300

    Update page-prototipo.php

--- themes/Nemo/page-prototipo.php
+++ themes/Nemo/page-prototipo.php
@@ -25,7 +25,12 @@ get_header();
 
             <div class="container text-sobre">
                 <div class="body-sobre">
-                
+                <p>
+                Implementação de um protótipo demonstrando uma integração e visão unificada sobre os dados distribuídos em vários silos, que permite a um gestor do Governo do ES obter respostas a questões que são possíveis apenas a partir da integração bem fundamentada dos dados dos diversos sistemas de informação do PCDV. 
+                </p>
+                <p>
+                O protótipo, disponível em <a href="http://dev.nemo.inf.ufes.br:8180/intersep/">http://dev.nemo.inf.ufes.br:8180/intersep/</a>(usuário: admin, senha: admin), utiliza dados fictícios, porém é baseado na estrutura real dos bancos de dados do DEON e do SIEP.
+                </p>
                 
                 
                 </div>
