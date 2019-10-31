9d17a8647bde8e34887ea0e775e7f28ef8a11de9
stiamat <38559112+stiamat@users.noreply.github.com>
2019-10-31 16:25:40 -0300

    Update section-conheca.php

--- themes/Nemo/sections/section-conheca.php
+++ themes/Nemo/sections/section-conheca.php
@@ -4,62 +4,76 @@
         <div class="titulo-conheca" class="container"> Conheça o projeto </div>
             <div class="card-deck">
                 <div class="card">
+                    <a target="_blank" href="http://dev.nemo.inf.ufes.br:8180/intersep/">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/mini-logo.svg" class="img-retangular">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Intersep </h5>
                     <div class="hr-default"> <hr class="hr-principal"></div>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
+                    </a>
                 </div>
                 <div class="card">
+                    <a href="<?php echo get_permalink( get_page_by_path( 'membros' ) ) ?>">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/network.svg" class="img-quadrada">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Membros</h5>
                     <div class="hr-default"> <hr class="hr-principal"></div>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
+                    </a>
                 </div>
                 <div class="card">
+                    <a href="<?php echo get_permalink( get_page_by_path( 'publicacoes' ) ) ?>">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/publications.svg" class="img-retangular">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Publicações</h5>
                     <div class="hr-default"> <hr class="hr-principal"></div>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
+                    </a>
                 </div>
             </div>
             <div class="card-deck">
                 <div class="card">
+                    <a href="<?php echo get_permalink( get_page_by_path( 'ontologia' ) ) ?>">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/connection.svg" class="img-quadrada">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Ontologia</h5>
                     <div class="hr-default"> <hr class="hr-principal"></div>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
+                    </a>
                 </div>
                 <div class="card">
+                    <a href="<?php echo get_permalink( get_page_by_path( 'abordagens' ) ) ?>">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/presentation.svg" class="img-quadrada">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Abordagens</h5>
                     <div class="hr-default"> <hr class="hr-principal"></div>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
+                    </a>
                 </div>
                 <div class="card">
+                    <a href="<?php echo get_permalink( get_page_by_path( 'prototipo' ) ) ?>">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/worker.svg" class="img-quadrada">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Protótipo</h5>
                     <div class="hr-default"> <hr class="hr-principal"></div>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
+                    </a>
                 </div>
                 <div class="card">
+                    <a href="<?php echo get_permalink( get_page_by_path( 'contato' ) ) ?>">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/discussion.svg" class="img-quadrada">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Contato</h5>
                     <div class="hr-default"> <hr class="hr-principal"></div>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
+                    </a>
                 </div>
             </div>
     </div>
