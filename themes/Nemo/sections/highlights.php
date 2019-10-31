fc146b5103e6337a70e4383dd992c26da473fa5a
stiamat <38559112+stiamat@users.noreply.github.com>
2019-10-31 12:49:16 -0300

    Update highlights.php

--- themes/Nemo/sections/highlights.php
+++ themes/Nemo/sections/highlights.php
@@ -8,8 +8,8 @@
                
             </div>
             <div class="div-button">
-                <a type="button" class="btn btn-outline-primary" href="<?php echo get_permalink( get_page_by_title( 'Sobre' ) ) ?>">DISCOVER</a>
-                <a type="button" class="btn btn-outline-secondary" href="<?php echo get_permalink( get_page_by_title( 'Contato' ) ) ?>">CONTACT US</a>
+                <a type="button" class="btn btn-outline-primary" href="<?php echo get_permalink( get_page_by_path( 'sobre' ) ) ?>">DISCOVER</a>
+                <a type="button" class="btn btn-outline-secondary" href="<?php echo get_permalink( get_page_by_path( 'contato' ) ) ?>">CONTACT US</a>
             </div>
                 <!-- <h1 class="display-7 text">Núcleo de Estudos em Modelagem Conceitual e Ontologias</h1>
                 <a class="btn btn-primary buttom-highlights" href="<?php echo get_permalink('sobre')?>" role="button">CONHEÇA O PROJETO</a> -->
