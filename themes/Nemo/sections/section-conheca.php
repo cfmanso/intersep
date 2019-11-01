2f3719c3365ff021d895a3371184a134d7dbf029
Carolina Manso <carolcarolmanso@gmail.com>
2019-11-01 09:09:54 -0300

    Update section-conheca.php

--- themes/Nemo/sections/section-conheca.php
+++ themes/Nemo/sections/section-conheca.php
@@ -2,8 +2,8 @@
 
     <div class="conheca-quadro" style="    background-color: transparent;">
         <div class="titulo-conheca" class="container"> Conheça o projeto </div>
-            <div class="card-deck">
-                <div class="card">
+            <div class="card-deck col-12">
+                <div class="card col-3">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/mini-logo.svg" class="img-retangular">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Intersep </h5>
@@ -11,7 +11,7 @@
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="card col-3">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/network.svg" class="img-quadrada">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Membros</h5>
@@ -19,7 +19,7 @@
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="card col-3">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/publications.svg" class="img-retangular">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Publicações</h5>
@@ -28,8 +28,8 @@
                     </div>
                 </div>
             </div>
-            <div class="card-deck">
-                <div class="card">
+            <div class="card-deck col-12">
+                <div class="card col-3">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/connection.svg" class="img-quadrada">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Ontologia</h5>
@@ -37,7 +37,7 @@
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="card col-3">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/presentation.svg" class="img-quadrada">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Abordagens</h5>
@@ -45,7 +45,7 @@
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="card col-3">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/worker.svg" class="img-quadrada">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Protótipo</h5>
@@ -53,7 +53,7 @@
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
                 </div>
-                <div class="card">
+                <div class="card col-3">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/discussion.svg" class="img-quadrada">
                     <div class="card-body">
                     <h5 class="subtitulo-conheca">Contato</h5>
